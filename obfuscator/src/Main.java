fc3f74f34555ec36a348a61ac058fafa78db18e4
dhslrl321 <dhslrl321@gmail.com>
2021-09-30 04:21:48 +0900

    commit

--- obfuscator/src/Main.java
+++ obfuscator/src/Main.java
@@ -18,25 +18,25 @@ public class Main {
         System.out.println("난독화를 수행할 Eicar Test 악성코드 command : " + malware + "\n");
 
         // 문자열 재배치
-        factory = new ObfuscatorFactory(ObfuscationType.REORDER);
-        obfuscator = factory.getObfuscator();
-        String reorder = obfuscator.obfuscate(malware);
-        System.out.println("[문자열 재배치가 적용된 난독화] = " + reorder);
-        System.out.println("[문자열 재배치 복구] = " + obfuscator.revert(reorder) + "\n\n");
+//        factory = new ObfuscatorFactory(ObfuscationType.REORDER);
+//        obfuscator = factory.getObfuscator();
+//        String reorder = obfuscator.obfuscate(malware);
+//        System.out.println("[문자열 재배치가 적용된 난독화] = " + reorder);
+//        System.out.println("[문자열 재배치 복구] = " + obfuscator.revert(reorder) + "\n\n");
 
         // Base 64 인코딩
-        factory = new ObfuscatorFactory(ObfuscationType.BASE64_ENCODING);
-        obfuscator = factory.getObfuscator();
-        String base64encoding = obfuscator.obfuscate(malware);
-        System.out.println("[base64encoding 이 적용된 난독화] = " + base64encoding);
-        System.out.println("[base64encoding 복구] = " + obfuscator.revert(base64encoding) + "\n\n");
+//        factory = new ObfuscatorFactory(ObfuscationType.BASE64_ENCODING);
+//        obfuscator = factory.getObfuscator();
+//        String base64encoding = obfuscator.obfuscate(malware);
+//        System.out.println("[base64encoding 이 적용된 난독화] = " + base64encoding);
+//        System.out.println("[base64encoding 복구] = " + obfuscator.revert(base64encoding) + "\n\n");
 
         // whitespace
-        factory = new ObfuscatorFactory(ObfuscationType.WHITESPACE);
-        obfuscator = factory.getObfuscator();
-        String whitespace = obfuscator.obfuscate(malware);
-        System.out.println("[whitespace 가 적용된 난독화] = " + whitespace);
-        System.out.println("[whitespace 복구] = " + obfuscator.revert(whitespace) + "\n\n");
+//        factory = new ObfuscatorFactory(ObfuscationType.WHITESPACE);
+//        obfuscator = factory.getObfuscator();
+//        String whitespace = obfuscator.obfuscate(malware);
+//        System.out.println("[whitespace 가 적용된 난독화] = " + whitespace);
+//        System.out.println("[whitespace 복구] = " + obfuscator.revert(whitespace) + "\n\n");
 
 
         CompositedObfuscation obfuscation = new CompositedObfuscation();
